ed5aa61de266a9470702dea48be8b43328c28907
CiaranR <ciaran.rooney@gmail.com>
2012-12-20 03:42:48 +0000

    updates to our wedding party

--- pages/party-tpl.php
+++ pages/party-tpl.php
@@ -20,7 +20,7 @@
 				<img src="./images/party/darren.jpg?v=<?php echo $version;?>" title="Darren Rooney" alt="Darren Rooney" class="left pad-right-15" />
 				<p class="text-left"><strong>Darren Rooney<br />
 				   Best Man</strong></p>
-				<p class="text-left">Bacon ipsum dolor sit amet spare ribs bacon boudin, salami pork sirloin filet mignon prosciutto pork loin shankle tail ground round meatball meatloaf. Ham hock </p>
+				<p class="text-left">Darren is Ciar&aacute;n's younger brother. He has long suffered from middle child syndrom and is now looking forward to his chance to shine. He has promised not to just download his speach from a webite, regardless of how appropriate that might be.</p>
 			</div>			
 		</div>
 		<div class="row">
@@ -31,10 +31,10 @@
 				<p class="text-right">Bacon ipsum dolor sit amet spare ribs bacon boudin, salami pork sirloin filet mignon prosciutto pork loin shankle tail ground round meatball meatloaf. Ham hock </p>
 			</div>
 			<div class="six columns">
-				<img src="./images/party/dave.jpg?v=<?php echo $version;?>" title="David Rooney" alt="David Rooney" class="left pad-right-15" />
-				<p class="text-left"><strong>David Rooney<br />
+				<img src="./images/party/ultan.jpg?v=<?php echo $version;?>" title="Ultan Rooney" alt="Ultan Rooney" class="left pad-right-15" />
+				<p class="text-left"><strong>Ultan Rooney<br />
 				   Groomsman</strong></p>
-				<p class="text-left">Bacon ipsum dolor sit amet spare ribs bacon boudin, salami pork sirloin filet mignon prosciutto pork loin shankle tail ground round meatball meatloaf. Ham hock</p>
+				<p class="text-left"><span title="aka Wee Man">Ultan</span> is Ciar&aacute;n's little brother. The youngest in the family he is used to getting things his own way, so he will have to listen to what his big brother says for once.</p>
 			</div>			
 		</div>
 		<div class="row">
@@ -45,11 +45,53 @@
 				<p class="text-right">Bacon ipsum dolor sit amet spare ribs bacon boudin, salami pork sirloin filet mignon prosciutto pork loin shankle tail ground round meatball meatloaf. Ham hock </p>
 			</div>
 			<div class="six columns">
+				<img src="./images/party/dave.jpg?v=<?php echo $version;?>" title="David Rooney" alt="David Rooney" class="left pad-right-15" />
+				<p class="text-left"><strong>David Rooney<br />
+				   Groomsman</strong></p>
+				<p class="text-left"><span title="aka Ted">Dave</span> and Ciar&aacute;n met doing the same course in DCU, they started a Snowboarding Club together, became great friends and lived together for years. They have been mistaken for brothers ever since they first met.</p>
+			</div>			
+		</div>		
+		<div class="row">
+			<div class="six columns">
+				<img src="./images/party/lisa.jpg?v=<?php echo $version;?>" title="Lisa D'ambrosio" alt="Lisa D'ambrosio" class="right pad-left-15" />
+				<p class="text-right"><strong>Lisa D'ambrosio<br />
+				   Bridesmaid</strong></p>
+				<p class="text-right">Bacon ipsum dolor sit amet spare ribs bacon boudin, salami pork sirloin filet mignon prosciutto pork loin shankle tail ground round meatball meatloaf. Ham hock </p>
+			</div>
+			<div class="six columns">
+				<img src="./images/party/mark.jpg?v=<?php echo $version;?>" title="Mark Kiernan" alt="Mark Kiernan" class="left pad-right-15" />
+				<p class="text-left"><strong>Mark  Kiernan<br />
+				   Groomsman</strong></p>
+				<p class="text-left"><span title="aka Stressed Eric">Mark</span> and Ciar&aacute;n also met doing the same course at DCU, he also started the Snowboarding Club with him. Since they first met Mark has always been one of Ciar&aacute;n's closest friends. Mark even went on to work with Ciar&aacute;n after University.</p>
+			</div>				
+		</div>
+		<div class="row">
+			<div class="six columns">
+				<img src="./images/party/sharkey.jpg?v=<?php echo $version;?>" title="Christine Valentic" alt="Christine Valentic" class="right pad-left-15" />
+				<p class="text-right"><strong>Christine Valentic<br />
+				   Bridesmaid</strong></p>
+				<p class="text-right">Bacon ipsum dolor sit amet spare ribs bacon boudin, salami pork sirloin filet mignon prosciutto pork loin shankle tail ground round meatball meatloaf. Ham hock </p>
+			</div>
+			<div class="six columns">
+				<img src="./images/party/alan.jpg?v=<?php echo $version;?>" title="Alan O'Conor" alt="Alan O'Conor" class="left pad-right-15" />
+				<p class="text-left"><strong>Alan O'Conor<br />
+				   Groomsman</strong></p>
+				<p class="text-left"><span title="aka Lad">Alan</span> and Ciar&aacute;n again met doing the same course at DCU, despite Alan aways being wrong their common love of the internet ment that they too became great friends. Alan also started the Snowboarding Club with Ciar&aacute;n and is now living the dream in San Fransisco.</p>
+			</div>			
+		</div>
+		<div class="row">
+			<div class="six columns">
+				<img src="./images/party/eliza.jpg?v=<?php echo $version;?>" title="Eliza Rafferty" alt="Eliza Rafferty" class="right pad-left-15" />
+				<p class="text-right"><strong>Eliza Rafferty<br />
+				   Bridesmaid</strong></p>
+				<p class="text-right">Bacon ipsum dolor sit amet spare ribs bacon boudin, salami pork sirloin filet mignon prosciutto pork loin shankle tail ground round meatball meatloaf. Ham hock </p>
+			</div>
+			<div class="six columns">
 				<img src="./images/party/russell.jpg?v=<?php echo $version;?>" title="Russell O'Connor" alt="Russell O'Connor" class="left pad-right-15" />
 				<p class="text-left"><strong>Russell O'Connor<br />
 				   Groomsman</strong></p>
-				<p class="text-left">Bacon ipsum dolor sit amet spare ribs bacon boudin, salami pork sirloin filet mignon prosciutto pork loin shankle tail ground round meatball meatloaf. Ham hock</p>
-			</div>			
+				<p class="text-left"><span title="aka Russell">Russell</span> is one of Ciar&aacute;n's closest friends, they met in DCU after they both became Student Union Reps and have been best friends since. Russell likewise ran the Snowboarding Club with Ciar&aacute;n and together they orginized some of the best events DCU has ever seen.</p>
+			</div>		
 		</div>
 	</div>
 	<!-- end content -->
